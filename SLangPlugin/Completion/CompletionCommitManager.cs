09a09f5622a312c2615647f5b64f39c1258d6e36
Maksim Surkov <modbrin@gmail.com>
2019-06-23 01:24:31 +0300

    Added AsyncCompletion sample code.

---
+++ SLangPlugin/Completion/CompletionCommitManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading;
+using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion;
+using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion.Data;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLangPlugin.Completion
+{
+    #region Manager Provider
+    [Export(typeof(IAsyncCompletionCommitManagerProvider))]
+    [Name("Chemical element commit manager provider")]
+    [ContentType("text")]
+    class SampleCompletionCommitManagerProvider : IAsyncCompletionCommitManagerProvider
+    {
+        IDictionary<ITextView, IAsyncCompletionCommitManager> cache = new Dictionary<ITextView, IAsyncCompletionCommitManager>();
+
+        public IAsyncCompletionCommitManager GetOrCreate(ITextView textView)
+        {
+            if (cache.TryGetValue(textView, out var itemSource))
+                return itemSource;
+
+            var manager = new SampleCompletionCommitManager();
+            textView.Closed += (o, e) => cache.Remove(textView); // clean up memory as files are closed
+            cache.Add(textView, manager);
+            return manager;
+        }
+    }
+    #endregion
+
+    #region Manager
+    /// <summary>
+    /// The simplest implementation of IAsyncCompletionCommitManager that provides Commit Characters and uses default behavior otherwise
+    /// </summary>
+    internal class SampleCompletionCommitManager : IAsyncCompletionCommitManager
+    {
+        public SampleCompletionCommitManager()
+        {
+        }
+
+        ImmutableArray<char> commitChars = new char[] { ' ', '\'', '"', ',', '.', ';', ':' }.ToImmutableArray();
+
+        public IEnumerable<char> PotentialCommitCharacters => commitChars;
+
+        public bool ShouldCommitCompletion(IAsyncCompletionSession session, SnapshotPoint location, char typedChar, CancellationToken token)
+        {
+            // This method runs synchronously, potentially before CompletionItem has been computed.
+            // The purpose of this method is to filter out characters not applicable at given location.
+
+            // This method is called only when typedChar is among the PotentialCommitCharacters
+            // in this simple example, all PotentialCommitCharacters do commit, so we always return true
+            return true;
+        }
+
+        public CommitResult TryCommit(IAsyncCompletionSession session, ITextBuffer buffer, CompletionItem item, char typedChar, CancellationToken token)
+        {
+            // Objects of interest here are session.TextView and session.TextView.Caret.
+            // This method runs synchronously
+
+            return CommitResult.Unhandled; // use default commit mechanism.
+        }
+    }
+    #endregion
+}
